3173e148af579e0aa632ad9b50546d30498d5de4
stardust <gokulgopalan18@gmail.com>
2025-05-27 10:33:24 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
-  ],
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['todoIcon.svg'],
+      manifest: {
+        name: "TaskTracker - To-Do List",
+        short_name: "TaskTracker",
+        description: "Stay productive with TaskTracker, your minimalist to-do list app.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#4f46e5",
+        icons: [
+          {
+            src: 'check-list128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'check-list.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]
 })
